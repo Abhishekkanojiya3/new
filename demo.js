d975507639e8d017e878bd920c41e426035c911c
Abhishekkanojiya3 <abhi67562>
2022-12-08 14:30:19 +0530

    getelementbytagname

--- demo.js
+++ demo.js
@@ -5,7 +5,7 @@ console.log(document.title);
 
 
 //console.log(document.getElementById('header-title'));
-var headerTitle = document.getElementById('header-title');
+//var headerTitle = document.getElementById('header-title');
 //var header = document.getElementById('main-header');
 //console.log(headerTitle);
 //headerTitle.textContent='Hello';
@@ -13,5 +13,17 @@ var headerTitle = document.getElementById('header-title');
 //console.log(headerTitle.innerText);
 //headerTitle.innerHTML='<h3>Hello</h3>';
 //header.style.borderBottom='solid 3px #000';
-document.getElementById('header-title').style.fontWeight = "bold";
-document.getElementById("header-title").style.color = "green";
+//document.getElementById('header-title').style.fontWeight = "bold";
+//document.getElementById("header-title").style.color = "green";
+
+//var items = document.getElementsByClassName('list-group-item');
+//console.log(items);
+ //items[3].style.backgroundColor = 'green';
+
+ //for(var i=0; i<items.length;i++){
+    //items[i].style.fontWeight='bold';
+ //}
+
+var li=document.getElementsByTagName('li');
+console.log(li);
+li[1].textContent='hello 2';
